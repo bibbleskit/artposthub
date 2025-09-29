b412d0818f17a2d8e72efd95ab870baeeb9a316a
bibbleskit <andseg@proton.me>
2025-09-29 11:09:25 -0700

    Added optional arguments and arg parser

--- autopost.py
+++ autopost.py
@@ -1,7 +1,33 @@
 import wordpress
+import argparse
 
-title = "Penny - Blue Dress"
-image = "/mnt/storage_drive/Andre Art/OC/Hare & Feather/Penny/penny-blue-dress-new.png" 
+#wplink = wordpress.post(title, image)
+#print(wplink)
 
-wplink = wordpress.post(title, image)
-print(wplink)
+parser = argparse.ArgumentParser()
+parser.add_argument("title", help="The title for the post.", type=str)
+parser.add_argument("image_path", help="The path to the image file.", type=str)
+parser.add_argument("-a", "--alttext", help="Alt text for the image.", type=str)
+parser.add_argument("-d", "--description", help="A description for the image.", type=str)
+parser.add_argument("-c", "--caption", help="A caption for the image.", type=str)
+parser.add_argument("--all", help="Enables all available modules.", action="store_true")
+parser.add_argument("-w", "--wordpress", help="Enables the WordPress module.", action="store_true")
+parser.add_argument("-b", "--bluesky", help="Enables the WordPress module.", action="store_true")
+parser.add_argument("-t", "--tumblr", help="Enables the WordPress module.", action="store_true")
+args = parser.parse_args()
+
+if args.all:
+    args.wordpress = True
+    args.bluesky = True
+    args.tumblr = True
+
+if args.wordpress:
+    try:
+        link = wordpress.post(args.title, args.image_path, description=args.description, alt_text=args.alttext)
+        print("WordPress Post URL: " + link)
+    except Exception as e:
+        print("ERROR: There was an issue posting to WordPress.")
+        print(f"Title: {args.title}")
+        print(f"Image Path: {args.image_path}")
+        print(e)
+
--- wordpress.py
+++ wordpress.py
@@ -3,60 +3,90 @@ from requests.auth import HTTPBasicAuth
 import config
 import onefolder
  
-def upload_image(img_path: str):
-    alt_text = onefolder.get_alt_text(img_path)
-    tags = onefolder.get_tags(img_path)
-    tag_ids = []
-    for tag in tags:
-        tag_ids.append(get_tag_id(tag))
-
-    url=config.wp_url + '/wp-json/wp/v2/media'
+# Uploads the image at the specified image_path.
+# Will update with provided alt_text
+# and description if provided.
+#
+# If not provided, it ill attempt to grab alt_text from
+# OneFolder information. See onefolder.py
+def upload_image(img_path: str, alt_text = '', description=''):
+    url=config.wp_url + '/media'
     auth = HTTPBasicAuth(config.wp_user, config.wp_key)
     filename=img_path.split('/')[-1]
 
+    image_info = None
     with open(img_path, 'rb') as image:
         res = requests.post(url=url,
                         data=image.read(),
                         headers={ 'Content-Type': 'image/png','Content-Disposition' : f"attachment; filename={filename}"},
                         auth=auth)
-        info = res.json()
-        image_info = {
-            'id':       info.get('id'),
-            'link':     info.get('guid').get('rendered'),
-            'alt_text': alt_text,
-            'tags': tag_ids
-            }
-        return image_info
+        image_info = res.json()
+
+    if not alt_text:
+        alt_text = onefolder.get_alt_text(img_path)
+
+    if alt_text or description:
+        post_data = {
+                'alt_text': alt_text,
+                'description': description
+                }
+        res = requests.post(url=url + f"/{image_info['id']}",
+                            json=post_data,
+                            headers={"Content-Type": "application/json"},
+                            auth=auth)
+        image_info = res.json()
+
+
+    return image_info
 
 def get_category_id(name):
-    url = config.wp_url + "/wp-json/wp/v2/categories?search=" + name
+    url = config.wp_url + '/categories?search=' + name
     auth = HTTPBasicAuth(config.wp_user, config.wp_key)
     
     res = requests.get(url, auth=auth)
     return res.json()[0]['id']
 
-def get_tag_id(name):
+# Returns the id of the given tag.
+# Will create the tag if it doesn't exist if create_if_missing is
+# set to true.
+#
+# Returns None if the tag doesn't exist and isn't created.
+#
+# Opinion: I think it should default to true because you can always
+# correction a typoed tag in WordPress when you see it. But if it
+# doesn't get added at all, you might forget.
+def get_tag_id(name: str, create_if_missing: bool = False) -> int:
     auth = HTTPBasicAuth(config.wp_user, config.wp_key)
+    return_id = None
 
+    # Try to return the tag id.
     try:
-        url = config.wp_url + "/wp-json/wp/v2/tags?search=" + name
+        url = config.wp_url + '/tags?search=' + name
         res = requests.get(url, auth=auth)
-        return res.json()[0]['id']
+        return_id = res.json()[0]['id']
+    # If it doesn't exist, create it and return the new id.
     except:
-        url = config.wp_url + "/wp-json/wp/v2/tags"
-        headers = {"Content-Type": "application/json"}
-        post_data = { "name": name }
-        res = requests.post(url, auth=auth, headers=headers, json=post_data)
-        return res.json()['id']
+        print(f"'{name}' doesn't appear to be a valid tag.")
+        if create_if_missing:
+            print("Creating the tag.")
+            url = config.wp_url + '/tags'
+            headers = {"Content-Type": "application/json"}
+            post_data = { "name": name }
+            res = requests.post(url, auth=auth, headers=headers, json=post_data)
+            return_id = res.json()['id']
 
-def post_image(title: str, image_info: str):
-    content = f"<!-- wp:image {{'id':{image_info['id']},'linkDestination':'custom','className':'wp-block-image size-large'}} -->\
-        <figure class='wp-block-image size-large'>\
-        <a href='{image_info['link']}'>\
-        <img src='{image_info['link']}' alt='{image_info['alt_text']}' class='wp-image-{image_info['id']}'/>\
-        </a></figure>\
-        <!-- /wp:image -->"
+    return return_id
 
+# Return a list of tag ids from strings.
+def get_tag_ids(tags: list[str]) -> list[int]:
+    return [get_tag_id(tag) for tag in tags if tag]
+
+
+# Creates a post using a dictionary of an image created by upload_image()
+# The data in image_info is the json returned by WordPress
+#
+# Returns the link of the post.
+def post_image(title: str, image_info: str, tag_ids=None) -> str:
     content = f"<!-- wp:image {{\"className\":\"size-large\"}} -->\
             <figure class=\"wp-block-image size-large\">\
                 <a href=\"{image_info['link']}\">\
@@ -65,7 +95,10 @@ def post_image(title: str, image_info: str):
             </figure>\
             <!-- /wp:image -->"
 
-    url = config.wp_url + "/wp-json/wp/v2/posts"
+    if image_info['description']['raw']:
+        content += f"<!-- wp:paragraph --><p>{image_info['description']['raw']}</p><!-- /wp:paragraph -->"
+
+    url = config.wp_url + '/posts'
     headers = {"Content-Type": "application/json"}
     auth = HTTPBasicAuth(config.wp_user, config.wp_key)
     post_data = {
@@ -73,17 +106,31 @@ def post_image(title: str, image_info: str):
         "content": content,
         "status": "publish",
         "featured_media": image_info['id'],
-        "tags": image_info['tags'],
+        "tags": tag_ids,
         "categories": [get_category_id('art')]
     }
 
     res = requests.post(url, headers=headers, auth=auth, json=post_data)
-    if res.status_code == 201:
-        return res.json()
-    else:
-        return False
+    # Returns the URL of the post.
+    # TODO: 
+    # Currently, if this doesn't work, and exception will be raised.
+    # This is good, but will interrupt any future modules being run.
+    # Should handle this properly in the future.
+    return res.json()['guid']['rendered']
 
-def post(title: str, image_path: str):
-    image_info = upload_image(image_path)
-    json = post_image(title, image_info)
-    return json['guid']['rendered']
+# Uploads an image and creates a WordPress post.
+# Returns a link to the post
+def post(title: str, image_path: str, tags: list[str] = None, alt_text: str = "", description : str = "") -> str:
+    # Upload the image to WordPress.
+    image_info = upload_image(image_path, alt_text=alt_text, description=description)
+
+    # Get tag ids from image.
+    if not tags:
+        tags = onefolder.get_tags(image_path)
+    tag_ids = get_tag_ids(tags)
+
+    # Create a post with the image as the main content and featured media.
+    post_link = post_image(title, image_info, tag_ids)
+
+    # Return the post link in case you want it.
+    return post_link
