2bc09688a7e5ee10201a49cb48798be997b79405
bibbleskit <andseg@proton.me>
2025-09-29 11:45:14 -0700

    example config

---
+++ config_example.py
@@ -0,0 +1,13 @@
+####
+## Wordpress Configuration Requirements
+####
+# The JSON API endpoint for the domain.
+wp_url  = 'https://YOUR.DOMAIN.TLD/wp-json/wp/v2'
+# The username the key is assigned to.
+wp_user = 'WP USERNAME'
+# The api key created from within WordPress.
+wp_key  = 'WP API KEY'
+# The category to upload to.
+wp_category = 'art'
+# If true, will create tags that do not exist.
+wp_create_tag_if_missing = True
